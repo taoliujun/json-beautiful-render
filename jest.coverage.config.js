b7d3ffc41070cb135a9a784b1a25a41b3608e2b9
taoliujun <taoliujun@qq.com>
2024-01-09 11:26:41 +0800

    test: organize test files

--- jest.coverage.config.js
+++ jest.coverage.config.js
@@ -4,10 +4,10 @@ const baseConfig = require('./jest.config');
 module.exports = {
     ...baseConfig,
 
-    testMatch: ['**/src/**/__tests__/**/*.ts', '!**/src/**/__tests__/**/*.snap.ts'],
+    testMatch: ['**/src/**/__tests__/**/*.ts', '!**/src/**/__tests__/**/*.snap.test.ts'],
 
     collectCoverage: true,
-    collectCoverageFrom: ['src/**/*.ts', '!src/**/*.snap.ts', '!src/index.ts'],
+    collectCoverageFrom: ['src/**/*.ts', '!src/**/*.snap.test.ts', '!src/index.ts'],
     coverageDirectory: 'reports/jest',
     coverageThreshold: { global: { lines: 90, branches: 50 } },
     coverageReporters: ['html'],
