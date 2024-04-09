66fa12a8dbc469a3b5c3750026c2078f18cff08b
taoliujun <taoliujun@qq.com>
2024-04-09 11:37:04 +0800

    fix: process is not defined

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,1 +1,7 @@
-export const IS_JEST = process.env.JEST_WORKER_ID !== undefined;
+let IS_JEST = false;
+
+try {
+    IS_JEST = process.env.JEST_WORKER_ID !== undefined;
+} catch {}
+
+export { IS_JEST };
